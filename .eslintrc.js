d9d603ba4dcbc97b3c6ac17c4afb57e12796eb9d
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-04-09 17:46:42 -0600

    Initial configurations - StackNavigator, react-native-vector-icons and project structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'comma-dangle': 'off',
+    'jsx-quotes': 'off',
+    semi: 'off',
+    'no-trailing-spaces': 'off',
+  },
 };
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import '../gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {StackNavigator} from './presentation/navigation/StackNavigator';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default App;
