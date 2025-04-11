4aab713fe18020521dcde3c44460a8659fb1fc4c
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-04-11 14:02:20 -0600

    Acciones de localización

--- src/presentation/screens/maps/MapsScreen.tsx
+++ src/presentation/screens/maps/MapsScreen.tsx
@@ -2,6 +2,10 @@ import {StyleSheet, View} from 'react-native';
 // import {Maps} from '../../components/maps/Maps';
 
 export const MapsScreen = () => {
+  // getCurrentLocation().then(location => {
+  //   console.log(location);
+  // });
+
   return <View style={styles.container}>{/* <Maps /> */}</View>;
 };
 
