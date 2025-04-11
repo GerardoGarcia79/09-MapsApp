2a5ceb509c3877b3500c05c0ca58b7a3830df031
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-04-11 12:14:05 -0600

    Configuración de GoogleMaps en Android

--- src/presentation/screens/maps/MapsScreen.tsx
+++ src/presentation/screens/maps/MapsScreen.tsx
@@ -1,9 +1,28 @@
-import {Text, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import {Platform, StyleSheet, View} from 'react-native';
 
 export const MapsScreen = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>MapsScreen</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={Platform.OS === 'ios' ? undefined : PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
