42e3b72de29b4125c6dc68dbfe415b1675e47062
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-04-11 13:38:53 -0600

    MapView - Mostrar la ubicación del usuario

--- src/presentation/components/maps/Maps.tsx
+++ src/presentation/components/maps/Maps.tsx
@@ -1,19 +1,26 @@
 import {Platform} from 'react-native';
-import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {Location} from '../../../infrastructure/interfaces/location';
 
-export const Maps = () => {
+interface Props {
+  showsUserLocation?: boolean;
+  initialLocation: Location;
+}
+
+export const Maps = ({showsUserLocation = true, initialLocation}: Props) => {
   return (
     <>
       <MapView
+        showsUserLocation={showsUserLocation}
         provider={Platform.OS === 'ios' ? undefined : PROVIDER_GOOGLE} // remove if not using Google Maps
         style={{flex: 1}}
         region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: initialLocation.latitude,
+          longitude: initialLocation.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}>
-        <Marker
+        {/* <Marker
           coordinate={{
             latitude: 37.78825,
             longitude: -122.4324,
@@ -21,7 +28,7 @@ export const Maps = () => {
           title="Titulo del marcador"
           description="Descripción del marcador"
           image={require('../../../assets/marker.png')}
-        />
+        /> */}
       </MapView>
     </>
   );
--- src/presentation/screens/maps/MapsScreen.tsx
+++ src/presentation/screens/maps/MapsScreen.tsx
@@ -1,12 +1,8 @@
 import {StyleSheet, View} from 'react-native';
-import {Maps} from '../../components/maps/Maps';
+// import {Maps} from '../../components/maps/Maps';
 
 export const MapsScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Maps />
-    </View>
-  );
+  return <View style={styles.container}>{/* <Maps /> */}</View>;
 };
 
 const styles = StyleSheet.create({
