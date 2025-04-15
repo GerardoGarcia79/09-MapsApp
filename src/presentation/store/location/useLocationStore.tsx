594e85f1c28d0dfd43b69e0c12d0597d9b8a7eb6
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-04-15 14:35:38 -0600

    Seguimiento de usuario - Store

--- src/presentation/store/location/useLocationStore.tsx
+++ src/presentation/store/location/useLocationStore.tsx
@@ -1,17 +1,50 @@
 import {create} from 'zustand';
 import {Location} from '../../../infrastructure/interfaces/location';
-import {getCurrentLocation} from '../../../actions/location/location';
+import {
+  getCurrentLocation,
+  clearWatchLocation,
+  watchCurrentLocation,
+} from '../../../actions/location/location';
 
 interface LocationState {
   lastKnownLocation: Location | null;
+  userLocations: Location[];
+  watchId: number | null;
   getLocation: () => Promise<Location | null>;
+  watchLocation: () => void;
+  clearWatchLocation: () => void;
 }
 
-export const useLocationStore = create<LocationState>()(set => ({
+export const useLocationStore = create<LocationState>()((set, get) => ({
   lastKnownLocation: null,
+  userLocations: [],
+  watchId: null,
+
   getLocation: async () => {
     const location = await getCurrentLocation();
     set(state => ({...state, lastKnownLocation: location}));
     return location;
+  },
+  watchLocation: () => {
+    const watchId = get().watchId;
+    if (watchId) {
+      get().clearWatchLocation();
+    }
+
+    const id = watchCurrentLocation(location => {
+      set(state => ({
+        ...state,
+        lastKnownLocation: location,
+        userLocations: [...state.userLocations, location],
+      }));
+    });
+
+    set(state => ({...state, watchId: id}));
+  },
+  clearWatchLocation: () => {
+    const watchId = get().watchId;
+    if (watchId) {
+      clearWatchLocation(watchId);
+    }
   },
 }));
